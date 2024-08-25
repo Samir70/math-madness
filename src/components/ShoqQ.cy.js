691e3b0a6f1c550b68f047bfae9769105b99688b
Samir70 <sbetmouni@talktalk.net>
2024-08-25 19:59:47 +0100

    TDD ShowQ

---
+++ src/components/ShoqQ.cy.js
@@ -0,0 +1,17 @@
+import ShowQ from "./ShowQ.vue";
+// import { mount } from "cypress/vue"
+describe('<ShowQ />', () => {
+  beforeEach(() => {
+    // see: https://on.cypress.io/mounting-vue
+    cy.mount(ShowQ, { props: { question: "What is 6 x 7?" } })
+  })
+
+  it("has a div for the current question text", () => {
+    cy.get('div#current-q').should('exist')
+    cy.get('div#current-q h2#q-text').should('exist')
+  })
+
+  it("shows the text passed as a prop", () => {
+    cy.get("h2#q-text").should('contain', "What is 6 x 7?")
+  })
+})
---
+++ src/components/ShowQ.vue
@@ -0,0 +1,22 @@
+<script setup>
+const props = defineProps({
+  question: String,
+})
+</script>
+
+<template>
+  <div id="current-q">
+    <h2 id="q-text">{{ props.question || "No question passed as props" }}</h2>
+  </div>
+</template>
+
+<style scoped>
+#current-q {
+  width: 80vw;
+  margin: auto;
+  text-align: center;
+  padding: 5px;
+  border: 1px solid black;
+  box-shadow: 5px 5px 1px 1px darkblue;
+}
+</style>
