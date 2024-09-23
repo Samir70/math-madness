cd146bacd2f0aa51e021ea44b6ef0a017b9d5b14
Samir70 <sbetmouni@talktalk.net>
2024-09-23 07:33:04 +0100

    add tests for ShowAs

---
+++ src/components/showAs.cy.js
@@ -0,0 +1,19 @@
+import ShowAs from "./ShowAs.vue";
+
+describe('<ShowAs />', () => {
+  beforeEach(() => {
+    // see: https://on.cypress.io/mounting-vue
+    cy.mount(ShowAs, { props: { answers: [1, 2, 42, 3, 4] } })
+  })
+  it("has a div for the answer options", () => {
+    cy.get('div#answer-options').should('exist')
+  })
+
+  it("has a div for each answer", () => {
+    cy.get("div#answer-0").should('contain', "1")
+    cy.get("div#answer-1").should('contain', "2")
+    cy.get("div#answer-2").should('contain', "42")
+    cy.get("div#answer-3").should('contain', "3")
+    cy.get("div#answer-4").should('contain', "4")
+  })
+})
