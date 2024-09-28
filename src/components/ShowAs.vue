4d0b04b5f89ea7a98b7b5b69d09e6926e2e6b56c
Samir70 <sbetmouni@talktalk.net>
2024-09-28 13:41:29 +0100

    add some styling to ShowAs

--- src/components/ShowAs.vue
+++ src/components/ShowAs.vue
@@ -1,7 +1,9 @@
 <script setup>
+import { ref } from 'vue';
 const props = defineProps({
   answers: [String, Number],
 })
+const optionClass = ref("mcq-option")
 </script>
 
 <template>
@@ -9,7 +11,8 @@ const props = defineProps({
     <div v-for="(ans, idx) in answers" 
       v-bind:key="idx" 
       v-bind:id="`answer-${idx}`"
-      v-on:click="$emit('userAns', ans)">
+      v-on:click="$emit('userAns', ans)"
+      v-bind:class="optionClass">
       {{ ans }}
     </div>
   </div>
@@ -18,5 +21,13 @@ const props = defineProps({
 <style scoped>
 #answer-options {
   font-size: 3rem;
+  display: flex;
+  justify-content: center;
+  width: 90vw;
+}
+.mcq-option {
+  border: 1px solid black;
+  margin: 0px 5px;
+  padding: 2px;
 }
 </style>
